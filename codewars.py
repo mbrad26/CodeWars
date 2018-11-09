f54163000a145e03a0bb58aaff6bb979ea4bc43d
Marius Brad <mbrad26@gmail.com>
2018-11-09 19:31:02 +0000

    codewars

--- codewars.py
+++ codewars.py
@@ -39,3 +39,13 @@ def xo(s):
     else:
         return False
 
+
+"""Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+"""
+
+
+def add_binary(a,b):
+    return bin(a + b)[2:]

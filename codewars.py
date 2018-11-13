84551dab1b546a11728e2a7826476ba282aa76fe
Marius Brad <mbrad26@gmail.com>
2018-11-13 13:48:17 +0000

    codewars

--- codewars.py
+++ codewars.py
@@ -293,7 +293,41 @@ def cakes(recipe, available):
         return min(results)
 
 
+"""Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which 
+are substrings of strings of a2.
 
+#Example 1: a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+#Example 2: a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+
+In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+
+Beware: r must be without duplicates.
+Don't mutate the inputs.
+"""
+
+
+# def in_array(array1, array2):
+#     r = []
+#     for substr in array1:
+#         for str in array2:
+#             if substr in str:
+#                 r.append(substr)
+#     return list(sorted(set(r)))
+
+def in_array(array1, array2):
+    return [substr for substr in sorted(set(array1)) if substr in ''.join(array2)]
 
 
 
